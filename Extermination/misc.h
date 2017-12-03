4913968459394ac5df831c277e38c3fe8071f632
Bradykin <shulmanbusiness@gmail.com>
2017-12-03 15:49:47 -0500

    Update
    Update

--- Extermination/misc.h
+++ Extermination/misc.h
@@ -5,22 +5,9 @@
 #include "resource_manager.h"
 
 namespace game {
-<<<<<<< HEAD
-	Resource *getResource(ResourceManager *resman, std::string name) {
-		Resource *res = resman->GetResource(name);
-		if (!res) {
-			throw(std::string("Could not find resource \"") + name + std::string("\""));
-		}
-		return res;
-	}
 
-	Laser *createLaserInstance(ResourceManager *resman) {
-		Resource *geom = getResource(resman, "LaserMesh");
-		Resource *mat = getResource(resman, "ShinyTextureMaterial");
-		Resource *tex = resman->GetResource("window");
-=======
 	Resource *getResource(ResourceManager *resman, std::string name);
->>>>>>> dev-v2
+
 
 	Laser *createLaserInstance(ResourceManager *resman);
 }
